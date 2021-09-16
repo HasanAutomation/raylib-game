afe9214a625d87b16d32ab64741e9f318cd83468
Hasan Ali <hasan.ali@lacritz.com>
2021-09-16 20:12:12 +0530

    Added reactangle

--- axegame.cpp
+++ axegame.cpp
@@ -4,15 +4,43 @@
 using namespace std;
 int main()
 {
-    int width = 1350;
-    int height = 800;
-    bool isEating{false};
-    int milesToDisneyLand{100};
-    InitWindow(width, height, "Hello World");
-    while (true)
+    int width{1350};
+    int height{800};
+
+    // Circle coordinates
+    int circle_x{675};
+    int circle_y{400};
+    int radius{100};
+
+    // Values for rectangle
+    int pos_x{300};
+    int pos_y{0};
+    int rec_width{50};
+    int rec_height{50};
+
+    InitWindow(width, height, "Axe Master");
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
     {
         BeginDrawing();
-        ClearBackground(RED);
+        ClearBackground(WHITE);
+        // Game logic begins
+        DrawCircle(circle_x, circle_y, radius, BLUE);
+        DrawRectangle(pos_x, pos_y, rec_width, rec_height, RED);
+
+        // Move the axe
+        if (pos_y < 800)
+            pos_y += 10;
+        if (IsKeyDown(KEY_D) && circle_x < 1350)
+        {
+            circle_x = circle_x + 10;
+        }
+        if (IsKeyDown(KEY_A) && circle_x > 0)
+        {
+            circle_x = circle_x - 10;
+        }
+
+        // Game logic ends
         EndDrawing();
     }
 }
